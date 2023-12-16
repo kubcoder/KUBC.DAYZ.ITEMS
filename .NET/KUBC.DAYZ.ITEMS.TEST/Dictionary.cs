8db3da7291ff3e4c19a68848991b1f8e192defd0
kot23rus <agorshenin@kubcoder.ru>
2023-12-16 15:13:20 +0300

    Проверил формирование ответа из запроса... робит нах

--- .NET/KUBC.DAYZ.ITEMS.TEST/Dictionary.cs
+++ .NET/KUBC.DAYZ.ITEMS.TEST/Dictionary.cs
@@ -1,4 +1,8 @@
 using KUBC.DAYZ.ITEMS.Dictionary;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
 namespace KUBC.DAYZ.ITEMS.TEST
 {
     [TestClass]
@@ -13,6 +17,22 @@ namespace KUBC.DAYZ.ITEMS.TEST
             var fi = new FileInfo("Profiles\\KUBC\\ITEMS\\Dictionary\\items.json");
             var dict = ItemsCollection.Load(fi);
             Assert.IsNotNull(dict);
+        }
+        /// <summary>
+        /// Тестируем работу запроса
+        /// </summary>
+        [TestMethod]
+        public void TestRequest()
+        {
+            var req = new DictionaryRequest() { Cats = ["Животные", "Еда"], Find="mUs" };
+            var resp = req.GetResponse(new DirectoryInfo("Profiles\\KUBC\\ITEMS\\Dictionary"));
+            Assert.IsNotNull(resp);
+            var o = new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic)
+            };
+            Console.WriteLine(JsonSerializer.Serialize<DictionaryResponse>(resp, o));
         }
     }
 }
--- .NET/KUBC.DAYZ.ITEMS/Dictionary/DictionaryRequest.cs
+++ .NET/KUBC.DAYZ.ITEMS/Dictionary/DictionaryRequest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -42,5 +43,68 @@ namespace KUBC.DAYZ.ITEMS.Dictionary
         /// </remarks>
         public string[] Cats { get; set; } = [];
 
+
+
+        /// <summary>
+        /// Получить ответ на данный запрос
+        /// </summary>
+        /// <param name="path">Описание папочки в которой будем искать нужные файлики</param>
+        /// <returns>Данные ответа, или null если в целевой папочке не нашли нифига</returns>
+        public DictionaryResponse? GetResponse(DirectoryInfo path)
+        {
+            var categories = Categories.Load(new FileInfo($"{path.FullName}\\{Categories.FILENAME}"));
+            if (categories == null) return null;
+            var items = ItemsCollection.Load(new FileInfo($"{path.FullName}\\{ItemsCollection.FILENAME}"));
+            if (items == null) return null;
+            DictionaryResponse response = new();
+            int i = 0;
+            while(i< items.Count ) 
+            {
+                var ci = items.ElementAt(i);
+                if (InSearchCondition(ci.Key, ci.Value))
+                    i++;
+                else
+                    items.Remove(ci.Key);
+
+            }
+            response.SummaryCount = items.Count;
+            response.Categories = items.CalculateCategories();
+            foreach(var c in categories)
+            {
+                if (!response.Categories.ContainsKey(c.ShowName))
+                {
+                    response.Categories.Add(c.ShowName, 0);
+                }
+            }
+            var ri = items.OrderBy(x => x.Key).Skip(Start).Take(Count);
+            foreach(var zy in ri)
+            {
+                response.Items.Add(zy.Key, zy.Value);
+            }
+            return response;
+        }
+        /// <summary>
+        /// Определить входит ли итем в условия поиска
+        /// </summary>
+        /// <param name="itemName">Имя итема</param>
+        /// <param name="itemDesc">Описание итема</param>
+        /// <returns></returns>
+        private bool InSearchCondition(string itemName, ItemDescription itemDesc)
+        {
+            if (Cats.Length > 0)
+            {
+                if (!Cats.Contains(itemDesc.Category))
+                    return false;
+            }
+            if (!string.IsNullOrEmpty(Find))
+            {
+                if (itemName.Contains(Find, StringComparison.OrdinalIgnoreCase))
+                    return true;
+                if (itemDesc.Description.Contains(Find, StringComparison.OrdinalIgnoreCase))
+                    return true;
+                return false;
+            }
+            return true;
+        }
     }
 }
