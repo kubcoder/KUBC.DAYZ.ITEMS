d942873ecddfcd53f6ccec914afd62f8a2fa77e7
agorshenin <agorshenin@agnet.site>
2026-02-24 21:51:49 +0300

    #21 Примерно тут узнали, что эффект дыма создается как то иначе нежели мы думали

--- Server/4_World/entities/vehicles/carscript.c
+++ Server/4_World/entities/vehicles/carscript.c
@@ -2,7 +2,73 @@ modded class CarScript
 {
     void ClearEngineDamage()
     {
+        SetSynchDirty();
         m_EngineDestroyed = false;
-        CleanupEffects();
+        if (m_WheelSmokeFx)
+        {
+            for (int i = 0; i < m_WheelSmokeFx.Count(); i++ )
+            {
+                Effect ps = m_WheelSmokeFx.Get(i);
+                if (ps)
+                {
+                    SEffectManager.DestroyEffect(ps);
+                }
+            }
+            m_WheelSmokeFx.Clear();
+        }
+        if (m_WheelSmokePtcFx)
+        {
+            m_WheelSmokePtcFx.Clear();
+        }
+		if (m_coolantFx)
+        {
+            KCItems.Log("Эффект m_coolantFx существует");
+            SEffectManager.DestroyEffect(m_coolantFx);
+        }
+        else
+        {
+            KCItems.Log("Эффект m_coolantFx НЕ существует");
+        }
+        if (m_exhaustFx)
+        {
+            KCItems.Log("Эффект m_exhaustFx существует");
+            SEffectManager.DestroyEffect(m_exhaustFx);
+        }
+        else
+        {
+            KCItems.Log("Эффект m_exhaustFx НЕ существует");
+        }
+		if (m_engineFx)
+        {
+            KCItems.Log("Эффект m_engineFx существует");
+            SEffectManager.DestroyEffect(m_engineFx);
+        }
+        else
+        {
+            KCItems.Log("Эффект m_engineFx НЕ существует");
+        }
+        SEffectManager.DestroyEffect(m_engineFx);
+	
+		SEffectManager.DestroyEffect(m_CrashSoundLight);
+		SEffectManager.DestroyEffect(m_CrashSoundHeavy);
+		SEffectManager.DestroyEffect(m_WindowSmall);
+		SEffectManager.DestroyEffect(m_WindowLarge);
+		CleanupSound(m_CarHornSoundEffect);
+        SetSynchDirty();
+        KCItems.Log("Попытались удалить все эффекты");
     }
+
+    override void CreateCarDestroyedEffect()
+	{
+        super.CreateCarDestroyedEffect();
+        KCItems.Log("Вызван эффект дыма мотора");
+        if (m_engineFx)
+        {
+            KCItems.Log("Эффект m_engineFx существует");
+        }
+        else
+        {
+            KCItems.Log("Эффект m_engineFx НЕ существует");
+        }
+	}
 }
